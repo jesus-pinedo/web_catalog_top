4a6fe7942dc97c846a5f9b7119fab37a0247ca55
Jesus <jesus.pinedo@walmart.com>
2022-08-21 20:19:23 -0500

    get products by catalog/shoes route

--- src/api/index.js
+++ src/api/index.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 class Api {
 
 	constructor() {
-		// this.url = 'http://localhost:3001'
+		 //this.url = 'http://localhost:3002'
 		this.url = 'https://top-quality-api.herokuapp.com'
 	}
  
 
 
 	getProducts() {
-		return axios.get(`${this.url}/product/list/all`)
+		return axios.get(`${this.url}/product/catalog/shoes`)
 	}
 
 }
